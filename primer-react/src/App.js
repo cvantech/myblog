207bede014d9d914e598c533393d8961821a3826
cvantech <cvsa14@gmail.com>
2022-06-30 21:02:30 -0500

    First commit

---
+++ primer-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Testimonio from "./components/Testimonio.js";
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos</h1>
+      <Testimonio />
+      </div>
+    </div>
+  );
+}
+
+export default App;
